16eb870521112d851f27af7b49d00255a619df25
Jan Jurca <jjurcaj@gmail.com>
2023-07-25 15:40:56 +0200

    init

---
+++ mlflow/first_setup.py
@@ -0,0 +1,26 @@
+from mlflow.server import get_app_client
+import sys
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'admin'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = 'password'
+
+tracking_uri = "http://localhost:5001/"
+
+auth_client = get_app_client("basic-auth", tracking_uri=tracking_uri)
+try:
+    auth_client.create_user(username="mhradis", password="password")
+except:
+    logger.info("User already exists")
+auth_client.update_user_admin(username="mhradis", is_admin=True)
+
+try:
+    auth_client.create_user(username="jjurca", password="password")
+except:
+    logger.info("User already exists")
+    
+auth_client.update_user_admin(username="jjurca", is_admin=True)
