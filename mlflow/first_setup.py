ea73b9317d1981fcb051a83c95d0e69069d9815b
Jan Jurca <jjurcaj@gmail.com>
2023-07-25 15:50:51 +0200

    simplification

--- mlflow/first_setup.py
+++ mlflow/first_setup.py
@@ -13,14 +13,8 @@ tracking_uri = "http://localhost:5001/"
 
 auth_client = get_app_client("basic-auth", tracking_uri=tracking_uri)
 try:
-    auth_client.create_user(username="mhradis", password="password")
+    auth_client.create_user(username="username", password="password")
 except:
     logger.info("User already exists")
-auth_client.update_user_admin(username="mhradis", is_admin=True)
+auth_client.update_user_admin(username="username", is_admin=True)
 
-try:
-    auth_client.create_user(username="jjurca", password="password")
-except:
-    logger.info("User already exists")
-    
-auth_client.update_user_admin(username="jjurca", is_admin=True)
