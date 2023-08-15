efa69da9208b5fb2b27dda3b8c9bfbd9042f8741
Jan Jurca <jjurcaj@gmail.com>
2023-08-15 13:53:53 +0200

    segmentation loader fix

--- segmentation/segment_train.py
+++ segmentation/segment_train.py
@@ -318,15 +318,37 @@ def inference(model, image_path, saveto=None):
 
 
 def load_data(manifest):
+    random.seed(42)
     data = []
     with open(manifest) as f:
         for line in f:
             data.append(json.loads(line))
     media_ids = [x["media_id"] for x in data]
 
-    train_media_ids, valid_media_ids = train_test_split(data, test_size=0.2)
-    valid_media_ids, test_media_ids = train_test_split(valid_media_ids, test_size=0.5)
+    counts_per_media = {}
+    for media_id in media_ids:
+        counts_per_media[media_id] = counts_per_media.get(media_id, 0) + 1
 
+    total_counts = sum(counts_per_media.values())
+
+    sorted_list_descending = sorted(counts_per_media.items(), key=lambda item: item[1], reverse=True)
+
+    train_media_ids = []
+    train_counts = 0
+    valid_media_ids = []
+    valid_counts = 0
+    test_media_ids = []
+    test_counts = 0
+    for media_id, count in sorted_list_descending:
+        if train_counts < 0.8 * total_counts:
+            train_media_ids.append(media_id)
+            train_counts += count
+        elif valid_counts < 0.1 * total_counts:
+            valid_media_ids.append(media_id)
+            valid_counts += count
+        else:
+            test_media_ids.append(media_id)
+            test_counts += count
     train_data = [x for x in data if x["media_id"] in train_media_ids]
     valid_data = [x for x in data if x["media_id"] in valid_media_ids]
     test_data = [x for x in data if x["media_id"] in test_media_ids]
@@ -362,7 +384,7 @@ if __name__ == '__main__':
         inference(model, args.inference, args.save_to, )
         exit()
 
-    train, valid, test = load_data(os.path.join(args.dataset, "manifest.json"))
+    train, valid, test = load_data(os.path.join(args.dataset, "all_endoscopy.jsonl"))
 
     train_dataloader = DataLoader(SegmentationDataset(args.dataset, train), batch_size=args.batch_size, shuffle=True, num_workers=args.n_cpu)
     valid_dataloader = DataLoader(SegmentationDataset(args.dataset, valid), batch_size=args.batch_size, shuffle=False, num_workers=args.n_cpu)
